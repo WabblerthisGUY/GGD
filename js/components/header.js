a0d70c6cf60f707bf1f0c9e8f3778f83a76b7ab7
Gymhgy <49801033+Gymhgy@users.noreply.github.com>
2022-11-08 19:46:00 -0800

    passcode system

--- js/components/header.js
+++ js/components/header.js
@@ -16,45 +16,24 @@
           padding-bottom: .5rem;
       }
     </style>
-    <nav class="navbar navbar-dark bg-primary fixed-top">
+    <nav class="navbar navbar-dark navbar-expand-sm bg-primary fixed-top">
       <div class="container-fluid">
         <div class="navbar-brand">
             <a href="index.html"><img src="img/GGD.webp" alt="" width="24" height="24" class="d-inline-block align-text-top"></a>
             <b><slot>Grey Ghost Division</slot></b>
         </div>
-
-        <a class="navbar-toggler" type="button" data-bs-toggle="offcanvas" href="#sidebar">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
           <span class="navbar-toggler-icon"></span>
-        </a>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link" href="#">About Us</a>
+            <a class="nav-link" href="#">Join Us</a>
+            <a class="nav-link" href="#">Cadet Portal</a>
+          </div>
+        </div>
       </div>
     </nav>
-    <div id="sidebar" class="bg-primary text-white px-4 pt-4 offcanvas offcanvas-end">
-      <h4>Grey Ghost Division & T.S. Kearsarge</h1>
-      <ul class="list-unstyled">
-        <li>
-          <a href="./">Home</a>
-        </li>
-        <li>
-          <a href="cadets.html">Cadets/Parents</a>
-          <ul class="list-unstyled ps-4">
-            <li><a href="pod.html">POD</a></li>
-            <li><a href="calendar.html">Calendar</a></li>
-            <li><a href="announcements.html">Announcements</a></li>
-            <li><a href="chainofcommand.html">Chain of Command</a></li>
-            <li><a href="supply.html">Supply</a></li>
-            <li><a href="resources.html">Resources</a></li>
-            <li><a href="https://quarterdeck.seacadets.org/">Quarterdeck</a></li>
-            <li><a href="payment.html">Payment</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="prospect.html">Prospective Cadets</a>
-        </li>
-        <li>
-          <a href="staff.html">Officers</a>
-        </li>
-      </ul>
-    </div>
     `;
     class Header extends HTMLElement {
       constructor() {
