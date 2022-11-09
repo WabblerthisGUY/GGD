672b8d81633ce5f75841b3a8deb2697819bedf8f
Gymhgy <49801033+Gymhgy@users.noreply.github.com>
2022-11-09 13:24:23 -0800

    uri issues

--- js/protected.js
+++ js/protected.js
@@ -1,5 +1,5 @@
 if(document.cookie.indexOf("authorized") === -1) {
     console.log(1);
 
-    window.location.replace(document.baseURI + "protected.html?redirect=" + encodeURIComponent(window.location.pathname+window.location.search));
+    window.location.replace(document.baseURI + "protected.html?redirect=" + encodeURIComponent(location.href.replace(document.baseURI, '')));
 }
